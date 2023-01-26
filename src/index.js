44615a33e4cb4b846d27fb97b33a80dc46dfd026
ShamanShoma <shamanshoma@icloud.com>
2023-01-27 00:39:02 +0500

    problem solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == [] || matrix == null) {
+    return [];
+  }
+
+  let evenSubarray;
+  let newArr = [];
+
+  let evenSubarrays = matrix.filter((_, index) => index %2 != 0);
+
+  for (evenSubarray of evenSubarrays) {
+    evenSubarray = evenSubarray.reverse();
+  }
+
+  for (let subArr of matrix) {
+    for (let elem of subArr) {
+      newArr.push(elem);
+    } 
+  }
+
+  return newArr;
 }
